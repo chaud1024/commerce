53767aaaa034b43ebe1d34d06200d07c90f7d128
chaud1024 <chaud1024@gmail.com>
2022-11-07 12:12:36 +0900

    eidt get-products api to display items corresponding each category

--- pages/api/get-products.ts
+++ pages/api/get-products.ts
@@ -3,11 +3,23 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-async function getProducts(skip: number, take: number) {
+async function getProducts(skip: number, take: number, category: number) {
+  const where =
+    category && category !== -1
+      ? {
+          where: {
+            category_id: category,
+          },
+        }
+      : undefined
   try {
     const response = await prisma.products.findMany({
       skip: skip,
       take: take,
+      ...where,
+      orderBy: {
+        price: 'asc', // ALL인 경우 가격순으로 나열
+      },
     })
     console.log('response', response)
     return response
@@ -25,7 +37,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const { skip, take } = req.query
+  const { skip, take, category } = req.query
 
   if (skip == null || take == null) {
     res.status(400).json({ message: 'no skip or take' })
@@ -33,7 +45,11 @@ export default async function handler(
   }
 
   try {
-    const products = await getProducts(Number(skip), Number(take))
+    const products = await getProducts(
+      Number(skip),
+      Number(take),
+      Number(category)
+    )
     res.status(200).json({ items: products, message: `Success` })
   } catch (error) {
     res.status(400).json({ message: `Failed` })
