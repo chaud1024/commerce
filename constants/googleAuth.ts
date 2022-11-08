38c0d628f5970c699024d5bb58e77cac32286156
chaud1024 <chaud1024@gmail.com>
2022-11-08 15:02:22 +0900

    add googleAuth

--- constants/googleAuth.ts
+++ constants/googleAuth.ts
@@ -1,2 +1,2 @@
-export const CLIENT_ID = process.env.CLIENT_ID
+export const CLIENT_ID = process.env.CLIENT_ID || ''
 export const CLIENT_SECRRET = process.env.CLIENT_SECRRET
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { QueryClient } from '@tanstack/query-core'
+import { GoogleOAuthProvider } from '@react-oauth/google'
+import { CLIENT_ID } from '../constants/googleAuth'
 
 export default function App({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient({
@@ -10,8 +12,10 @@ export default function App({ Component, pageProps }: AppProps) {
     },
   })
   return (
-    <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
-    </QueryClientProvider>
+    <GoogleOAuthProvider clientId={`${process.env.CLIENT_ID}`}>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </GoogleOAuthProvider>
   )
 }
