e0a7f70201810e0c302fd7cd4ee9e80d7037ee3c
chaud1024 <chaud1024@gmail.com>
2022-11-08 15:02:33 +0900

    add google auth

---
+++ pages/api/auth/get-token.ts
@@ -0,0 +1,33 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { PrismaClient } from '@prisma/client'
+import jwtDecode from 'jwt-decode'
+
+const prisma = new PrismaClient()
+
+async function getToken(credential: string) {
+  const decoded = jwtDecode(credential)
+  try {
+    console.log('response', decoded)
+    return decoded
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+type Data = {
+  items?: any
+  message: string
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const { credential } = req.query
+  try {
+    const products = await getToken(String(credential))
+    res.status(200).json({ items: products, message: `Success` })
+  } catch (error) {
+    res.status(400).json({ message: `Failed` })
+  }
+}
